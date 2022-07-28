f705437b3288859871df869869e0fb7c0a6fd829
yotsufe <yotsufe@gmail.com>
2022-07-29 01:23:37 +0900

    背景設置

--- iOSSwiftUIResearch/iOSSwiftUIResearch/ThirdApp/ThirdAppView.swift
+++ iOSSwiftUIResearch/iOSSwiftUIResearch/ThirdApp/ThirdAppView.swift
@@ -4,7 +4,12 @@ import SwiftUI
 
 struct ThirdAppView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image("background")
+                .resizable()
+                .ignoresSafeArea()
+                .aspectRatio(contentMode: .fill)
+        }
     }
 }
 
