501a9b823cf2035dbc816a8b4ef0f84140fb04f9
yotsufe <yotsufe@gmail.com>
2022-07-31 02:30:10 +0900

    TextField追加

--- iOSSwiftUIResearch/iOSSwiftUIResearch/FourthApp/FourthAppView.swift
+++ iOSSwiftUIResearch/iOSSwiftUIResearch/FourthApp/FourthAppView.swift
@@ -3,8 +3,21 @@
 import SwiftUI
 
 struct FourthAppView: View {
+
+    @State var inputText: String = ""
+    @State var dispSearchKey: String = ""
+
     var body: some View {
-        MapView(searchKey: "東京駅")
+        VStack {
+            TextField(
+                "キーワードを入力してください",
+                text: $inputText,
+                onCommit: { dispSearchKey = inputText }
+            )
+            .padding()
+
+            MapView(searchKey: dispSearchKey)
+        }
     }
 }
 
