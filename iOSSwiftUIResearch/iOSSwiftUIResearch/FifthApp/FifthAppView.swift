cb4e6fa6ef17b22f6d14db246918a196bc032672
yotsufe <yotsufe@gmail.com>
2022-08-23 00:37:02 +0900

    パーツ設置

--- iOSSwiftUIResearch/iOSSwiftUIResearch/FifthApp/FifthAppView.swift
+++ iOSSwiftUIResearch/iOSSwiftUIResearch/FifthApp/FifthAppView.swift
@@ -4,7 +4,30 @@ import SwiftUI
 
 struct FifthAppView: View {
     var body: some View {
-        VStack {
+        VStack(spacing: 30.0) {
+            Text("残り10秒")
+                .font(.largeTitle)
+
+            HStack {
+                Button(action: {
+                }) {
+                    Text("スタート")
+                        .font(.title)
+                        .foregroundColor(Color.white)
+                        .frame(width: 140, height: 140)
+                        .background(Color("startColor"))
+                        .clipShape(Circle())
+                }
+                Button(action: {
+                }) {
+                    Text("ストップ")
+                        .font(.title)
+                        .foregroundColor(Color.white)
+                        .frame(width: 140, height: 140)
+                        .background(Color("stopColor"))
+                        .clipShape(Circle())
+                }
+            }
         }
         .toolbar {
             ToolbarItem(placement: .navigationBarTrailing) {
