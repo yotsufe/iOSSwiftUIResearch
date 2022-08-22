f90390172ce0050f85556850a30a5fafec222e99
yotsufe <yotsufe@gmail.com>
2022-08-22 20:38:06 +0900

    タイマー設定画面追加

--- iOSSwiftUIResearch/iOSSwiftUIResearch/FifthApp/FifthAppView.swift
+++ iOSSwiftUIResearch/iOSSwiftUIResearch/FifthApp/FifthAppView.swift
@@ -4,7 +4,15 @@ import SwiftUI
 
 struct FifthAppView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+        }
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                NavigationLink(destination: SettingView()) {
+                    Text("タイマー設定")
+                }
+            }
+        }
     }
 }
 
---
+++ iOSSwiftUIResearch/iOSSwiftUIResearch/FifthApp/SettingView.swift
@@ -0,0 +1,15 @@
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/).ignoresSafeArea()
+    }
+}
+
+struct SettingView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingView()
+    }
+}
