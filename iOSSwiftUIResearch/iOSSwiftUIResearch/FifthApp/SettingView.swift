85634a0c6904eed9073e0c1ea9cf109c2f88572b
yotsufe <yotsufe@gmail.com>
2022-08-23 22:35:57 +0900

    背景変更

--- iOSSwiftUIResearch/iOSSwiftUIResearch/FifthApp/SettingView.swift
+++ iOSSwiftUIResearch/iOSSwiftUIResearch/FifthApp/SettingView.swift
@@ -4,7 +4,10 @@ import SwiftUI
 
 struct SettingView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/).ignoresSafeArea()
+        ZStack {
+            Color("backgroundSetting")
+                .ignoresSafeArea()
+        }
     }
 }
 
