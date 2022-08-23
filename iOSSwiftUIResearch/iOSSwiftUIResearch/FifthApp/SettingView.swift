3709109181bd83576e5317f27efe00fac323c89e
yotsufe <yotsufe@gmail.com>
2022-08-23 23:14:59 +0900

    Picker追加

--- iOSSwiftUIResearch/iOSSwiftUIResearch/FifthApp/SettingView.swift
+++ iOSSwiftUIResearch/iOSSwiftUIResearch/FifthApp/SettingView.swift
@@ -3,10 +3,33 @@
 import SwiftUI
 
 struct SettingView: View {
+
+    @State var timerValue = 10
+
     var body: some View {
         ZStack {
             Color("backgroundSetting")
                 .ignoresSafeArea()
+
+            VStack {
+                Picker(selection: $timerValue) {
+                    Text("10")
+                        .tag(10)
+                    Text("20")
+                        .tag(20)
+                    Text("30")
+                        .tag(30)
+                    Text("40")
+                        .tag(40)
+                    Text("50")
+                        .tag(50)
+                    Text("60")
+                        .tag(60)
+                } label: {
+                    Text("選択")
+                }
+                .pickerStyle(.wheel)
+            }
         }
     }
 }
